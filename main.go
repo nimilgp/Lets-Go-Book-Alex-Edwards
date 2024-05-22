b4ef9d10a0755a33c243b5daeace8d130f7c382e
nimilgp <nimilgeorgepradeepngp@gmail.com>
2024-05-23 00:06:08 +0530

    added 2 more basic routes

--- main.go
+++ main.go
@@ -10,12 +10,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello from snippet-box!!!"))
 }
 
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("View snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Create snippet"))
+}
+
 func main() {
 		//create a new serveMux
 		mux := http.NewServeMux()
 
 		//register url patterns with handlers
 		mux.HandleFunc("/", home)
+		mux.HandleFunc("/snippet/view", snippetView)
+		mux.HandleFunc("/snippet/create", snippetCreate)
 
 		log.Println("Server is up!")
 		//start a new web server at a port, handled by a serveMux
