20ce817caef3458c4026113f50239ef96496b4a4
nimilgp <nimilgeorgepradeepngp@gmail.com>
2024-06-02 10:06:00 +0530

    update the home handler to use the base & home tmpl, also used ExecuteTemplate to specify respond with base.tmpl

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,14 +15,21 @@ func getHome(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Program", "Snippet-Box") 
 		w.Header().Add("Field-Name", "Can't have spaces in them")
 
-		ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+		//slice containing files to build the final home page
+		//base template must be 1st
+		files := []string{
+				"./ui/html/base.tmpl.html",
+				"./ui/html/pages/home.tmpl.html",
+		}
+
+		ts, err := template.ParseFiles(files...)//pass contents of file as variadic args
 		if err != nil {
 				log.Print(err.Error()) //log err on server system
 				http.Error(w, "Internal Server ERROR(ParseFiles)", http.StatusInternalServerError) //notify user of err
 				return
 		}
 
-		err = ts.Execute(w, nil)//write template set to w response body, dynamic data -> nil
+		err = ts.ExecuteTemplate(w, "base", nil)//specifically tells to respond with content 'base', dynamic data -> nil
 		if err != nil {
 				log.Print(err.Error())
 				http.Error(w, "Internal Server ERROR(Execute)", http.StatusInternalServerError)
