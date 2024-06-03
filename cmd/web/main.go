fd43d6b78be9ec59bd8b5494be7201fc925596f0
nimilgp <nimilgeorgepradeepngp@gmail.com>
2024-06-03 13:37:01 +0530

    added fileserver to serve files in /static/

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,12 @@ func main() {
 		//create a new serveMux
 		mux := http.NewServeMux()
 
+		//create file server
+		fileServer := http.FileServer(http.Dir("./ui/static/"))
+		
+		//register mux.Handle func to register yje file server asd the handler for url path /static
+		mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 		//register url patterns with handlers
 		mux.HandleFunc("GET /{$}", getHome)
 		mux.HandleFunc("GET /snippet/view/{id}", getSnippetView)
