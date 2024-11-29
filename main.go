473c5a8fdea1b576bfdd892f005f978daca7daa4
nimil <nimilgeorgepradeepngp@gmail.com>
2024-11-30 00:27:23 +0530

    3 basic routes

--- main.go
+++ main.go
@@ -9,9 +9,20 @@ func root(w http.ResponseWriter, r *http.Request) {
   w.Write([]byte("<h1>hello from webserver!</h1>"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("<h1>View specific snippet</h1>"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("<h1>Create a snippet</h1>"))
+}
+
 func main(){
   mux := http.NewServeMux()
+
   mux.HandleFunc("/", root)
+  mux.HandleFunc("/snippet/view", snippetView)
+  mux.HandleFunc("/snippet/create", snippetCreate)
 
   log.Println("Hello, world!")
 
