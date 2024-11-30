b0f9da827599c0a53aa3e8d268760ddb23f86308
nimil <nimilgeorgepradeepngp@gmail.com>
2024-12-01 00:36:45 +0530

    flag options to set port and static dir location

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
-	PORT := ":3333"
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", "3333", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static/", "Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 
 	mux.Handle("GET /static/", http.StripPrefix("/static/", fileServer))
 	mux.HandleFunc("GET /{$}", getRoot)
@@ -16,9 +26,9 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", getSnippetCreate)
 	mux.HandleFunc("POST /snippet/create", postSnippetCreate)
 
-	log.Println("server running at port", PORT)
+	log.Println("server running at port:", cfg.addr)
 
-	if err := http.ListenAndServe(PORT, mux); err != nil {
+	if err := http.ListenAndServe(":"+cfg.addr, mux); err != nil {
 		log.Fatal(err)
 	}
 }
