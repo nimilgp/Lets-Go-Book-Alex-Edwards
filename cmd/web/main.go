25bcb5cda53445a3dfe6f4a2e60107a2dc7cba0b
nimil <nimilgeorgepradeepngp@gmail.com>
2024-12-01 17:53:41 +0530

    database connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type config struct {
 	addr      string
 	staticDir string
+	dsn       string
 }
 
 type application struct {
@@ -20,10 +24,18 @@ func main() {
 	var cfg config
 	flag.StringVar(&cfg.addr, "addr", "3333", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static/", "Path to static assets")
+	flag.StringVar(&cfg.dsn, "dsn", "web:super-secret-passwd@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	var app application
 	app.logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		app.logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
 
 	app.logger.Info("starting server at port:", "addr", cfg.addr)
 
@@ -31,4 +43,19 @@ func main() {
 		app.logger.Error(err.Error())
 		os.Exit(-1)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
