92308ca100a37a91c6e0137b28bf043155f897dc
nimil <nimilgeorgepradeepngp@gmail.com>
2024-12-03 13:29:31 +0530

    snippets templates added for snippet list

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,25 +19,27 @@ func (app *application) getRoot(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, r, err)
 		return
 	}
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
+
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/root.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
 	}
-	// files := []string{
-	// 	"./ui/html/base.tmpl.html",
-	// 	"./ui/html/pages/root.tmpl.html",
-	// 	"./ui/html/partials/nav.tmpl.html",
-	// }
 
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w, r, err)
-	// 	return
-	// }
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
 
-	// err = ts.ExecuteTemplate(w, "base", nil)
-	// if err != nil {
-	// 	app.serverError(w, r, err)
-	// }
+	data := templateData{
+		Snippets: snippets,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+	}
 }
 
 func (app *application) getSnippetView(w http.ResponseWriter, r *http.Request) {
