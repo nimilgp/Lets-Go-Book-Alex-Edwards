55c2b831a5b8170298232bfa16f9ba5b17df5941
nimil <nimilgeorgepradeepngp@gmail.com>
2024-12-22 17:07:37 +0530

    restricted routes for unauth users

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,16 +16,18 @@ func (app *application) route(cfg config) http.Handler {
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.getRoot))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.getSnippetView))
-	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.getSnippetCreate))
-	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.postSnippetCreate))
 
 	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.getUserSignup))
 	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.postUserSignup))
 	mux.Handle("GET /user/login", dynamic.ThenFunc(app.getUserLogin))
 	mux.Handle("POST /user/login", dynamic.ThenFunc(app.postUserLogin))
-	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.postUserLogout))
+
+	protected := dynamic.Append(app.requireAuthentication)
+
+	mux.Handle("GET /snippet/create", protected.ThenFunc(app.getSnippetCreate))
+	mux.Handle("POST /snippet/create", protected.ThenFunc(app.postSnippetCreate))
+	mux.Handle("POST /user/logout", protected.ThenFunc(app.postUserLogout))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
-
 	return standard.Then(mux)
 }
